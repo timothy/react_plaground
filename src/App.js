96a0c4e6d5b3a5835e3832e6abb117629b294dd6
Timothy B <b85205@gmail.com>
2017-10-04 10:13:38 -0400

    added simple button increment program

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import ReactDOM from 'react-dom';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {counter: 0};
+
+    incrementCounter = (incVal) => {
+        this.setState((preveState) => {
+            return {
+                counter: preveState.counter + incVal
+            }
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <Button onClickFunction={this.incrementCounter} incrementValue={1}/>
+                <Button onClickFunction={this.incrementCounter} incrementValue={5}/>
+                <Button onClickFunction={this.incrementCounter} incrementValue={10}/>
+                <Button onClickFunction={this.incrementCounter} incrementValue={100}/>
+                <Result counter={this.state.counter}/>
+            </div>
+        );
+    }
 }
+
+/*
+ First simple component built
+ */
+class Button extends Component {
+
+    handleClick = () => {
+        this.props.onClickFunction(this.props.incrementValue);
+    };
+
+    render() {
+        return (
+            <button onClick={this.handleClick} className="Test">
+                +{this.props.incrementValue}
+            </button>
+        );
+    }
+}
+
+const Result = (props) => {
+    return (
+        <div>{props.counter}</div>
+    );
+};
+
 
 export default App;
